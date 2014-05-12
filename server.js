f45af18d79445fada570ecf7e40fdca6d535b577
Steven Farthing <sfarthin@gmail.com>
2014-05-12 17:22:05 -0400

    add requirejs removal

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var server = prerender({
 server.use(prerender.blacklist());
 // server.use(prerender.logger());
 //server.use(prerender.removeScriptTags());
+server.use(require("./lib/plugins/removeRequirejsStuff"));
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
